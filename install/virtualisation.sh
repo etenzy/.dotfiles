975d2915a8c992e784caaafbc73c6230561341f7
Michael Rödel <hello@mroedel.de>
2021-12-15 10:35:15 +0100

    feat: add utm

--- install/virtualisation.sh
+++ install/virtualisation.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [[ "$ENABLE_INSTALL_UTM" == "true" ]]; then
+    echo ''
+    echo 'Install UTM'
+    echo '-----------'
+    brew install --cask utm
+fi
+
 if [[ "$ENABLE_INSTALL_VIRTUALBOX" == "true" ]]; then
     echo ''
     echo 'Install Virtualbox'
