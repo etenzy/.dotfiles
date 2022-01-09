affc7da4690263c16fc90ddeeefa78a196b15c23
Michael Rödel <hello@mroedel.de>
2022-01-09 01:39:24 +0100

    feat: add colima

--- install/container.sh
+++ install/container.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [[ "$ENABLE_INSTALL_COLIMA" == "true" ]]; then
+    echo ''
+    echo 'Install colima'
+    echo '--------------'
+    brew install docker docker-compose docker-credential-helper
+    brew install colima
+fi
+
 if [[ "$ENABLE_INSTALL_PODMAN" == "true" ]]; then
     echo ''
     echo 'Install podman'
