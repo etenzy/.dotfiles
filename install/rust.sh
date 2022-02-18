765fd8efa94fa23b98eed31ec1a63266b403fdb1
Michael Rödel <hello@mroedel.de>
2022-02-18 17:14:26 +0100

    feat: add rust

---
+++ install/rust.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ "$ENABLE_INSTALL_RUST" == "true" ]]; then
+    echo ''
+    echo 'Install rustup-init'
+    echo '-------------------'
+    brew install rustup-init
+    rustup-init -y
+fi
