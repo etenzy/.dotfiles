41f27cdbd8e864f4249611e7f2ee60c46f0dd61b
Michael Rödel <hello@mroedel.de>
2025-02-05 11:40:01 +0100

    feat(kubernetes): more debug functions

--- kubernetes/test-connection-pg.sh
+++ kubernetes/test-connection-pg.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env sh
 
-for ip in IPS; do 
-    for port in PORTS; do
+ips="IPS"
+ports="PORTS"
+database="DATABASE"
+user="USER"
+
+for ip in $ips; do 
+    for port in $ports; do
         printf "$(date '+%Y-%m-%d %H:%M:%S') | pg_isready on $ip:$port => "
-        pg_isready -h $ip -p $port -d DATABASE -U USER &> /dev/null \
+        pg_isready -h $ip -p $port -d $database -U $user &> /dev/null \
             && printf "\e[92msuccess\e[0m\n" \
             || printf "\e[91mfailed\e[0m\n" 
     done 
---
+++ kubernetes/test-connection-smb.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env 
+
+ip="IP"
+share="SHARE"
+user="USER"
+password="PASSWORD"
+
+sharestring="\\\\\\\\$ip\\\\$share\\\\"
+
+printf "$(date '+%Y-%m-%d %H:%M:%S') | smbclient on ${sharestring//\\\\/\\\\\\\\} => "
+    smbclient --user "$user" --password "$password" "$sharestring" -c 'listconnect' &> /dev/null \
+        && printf "\e[92msuccess\e[0m\n" \
+        || printf "\e[91mfailed\e[0m\n"
