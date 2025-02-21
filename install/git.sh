219ec2809e65f64593085a1e74550cae7b5613fc
Michael Rödel <hello@mroedel.de>
2025-02-21 14:44:45 +0100

    fix(git)

--- install/git.sh
+++ install/git.sh
@@ -9,7 +9,6 @@ if [[ "$ENABLE_INSTALL_GIT_LATEST" == "true" ]]; then
     echo ''
     echo 'Configure Git'
     echo '-------------'
-    brew install meteor
     rm $HOME/.gitconfig
     cp "$HOME/.dotfiles/git/gitconfig" "$HOME/.gitconfig"
     rm $HOME/.gitconfig-default
