6f7c86e982ed6ba8a6757cf7e8a50b8e8cad943d
Michael Rödel <hello@mroedel.de>
2025-03-07 18:21:54 +0100

    feat(functions): remove double escaping because smbclient accepts /

--- kubernetes/test-connection-smb.sh
+++ kubernetes/test-connection-smb.sh
@@ -5,9 +5,9 @@ share="SHARE"
 user="USER"
 password="PASSWORD"
 
-sharestring="\\\\$ip\\$share\\"
+sharestring="//$ip/$share/"
 
-printf "$(date '+%Y-%m-%d %H:%M:%S') | smbclient on ${sharestring//\\/\\\\} => "
+printf "$(date '+%Y-%m-%d %H:%M:%S') | smbclient on $sharestring => "
     smbclient --user "$user" --password "$password" "$sharestring" -c 'listconnect' &> /dev/null \
         && printf "\e[92msuccess\e[0m\n" \
         || printf "\e[91mfailed\e[0m\n"
