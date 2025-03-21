7b02ee64c251b4a15c647c859416dd5c9443f9ce
Michael Rödel <hello@mroedel.de>
2025-03-21 16:49:05 +0100

    feat(install/crontab): add cron config

---
+++ install/crontab.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [[ "$ENABLE_INSTALL_CRONJOB_WEEKLY" == "true" ]]; then
+    echo ''
+    echo 'Install weekly cronjob'
+    echo '----------------------'
+    echo "0 9 * * 1 /Users/$(whoami)/.dotfiles/jobs/weekly > /dev/null 2>&1" | sudo tee -a "/var/at/tabs/$(whoami)"
+fi
+
+if [[ "$ENABLE_INSTALL_CRONJOB_DAILY" == "true" ]]; then
+    echo ''
+    echo 'Install daily cronjob'
+    echo '---------------------'
+    echo "0 9 * * 2-5 /Users/$(whoami)/.dotfiles/jobs/daily > /dev/null 2>&1" | sudo tee -a "/var/at/tabs/$(whoami)"
+fi
