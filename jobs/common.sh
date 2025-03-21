4a33eaaf541a5e97755197cb76f7fdb31a0940b5
Michael Rödel <hello@mroedel.de>
2025-03-21 16:49:35 +0100

    feat(jobs): wip jobs

---
+++ jobs/common.sh
@@ -0,0 +1,5 @@
+export HOMEBREW_PREFIX="/opt/homebrew";
+export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
+export HOMEBREW_REPOSITORY="/opt/homebrew";
+
+export PATH="$HOMEBREW_PREFIX/bin:$HOMEBREW_PREFIX/sbin:$PATH"
