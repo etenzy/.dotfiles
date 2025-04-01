c8b48f0f3e76b1862a3b1265cdee34f5fa508099
Michael Rödel <hello@mroedel.de>
2025-04-01 11:37:52 +0200

    fix(crontab): postpone 15min

--- install/crontab.sh
+++ install/crontab.sh
@@ -4,12 +4,12 @@ if [[ "$ENABLE_INSTALL_CRONJOB_WEEKLY" == "true" ]]; then
     echo ''
     echo 'Install weekly cronjob'
     echo '----------------------'
-    echo "0 9 * * 1 /Users/$(whoami)/.dotfiles/jobs/weekly >> /dev/null 2>&1" | sudo tee -a "/var/at/tabs/$(whoami)"
+    echo "15 9 * * 1 /Users/$(whoami)/.dotfiles/jobs/weekly >> /dev/null 2>&1" | sudo tee -a "/var/at/tabs/$(whoami)"
 fi
 
 if [[ "$ENABLE_INSTALL_CRONJOB_DAILY" == "true" ]]; then
     echo ''
     echo 'Install daily cronjob'
     echo '---------------------'
-    echo "0 9 * * 2-5 /Users/$(whoami)/.dotfiles/jobs/daily >> /dev/null 2>&1" | sudo tee -a "/var/at/tabs/$(whoami)"
+    echo "15 9 * * 2-5 /Users/$(whoami)/.dotfiles/jobs/daily >> /dev/null 2>&1" | sudo tee -a "/var/at/tabs/$(whoami)"
 fi
