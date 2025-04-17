677cb11ed2ab242e17d10cc5b66614763f8b2bc2
Michael Rödel <hello@mroedel.de>
2025-04-17 22:48:28 +0200

    feat(fish): add loader for custom addons

--- shell/config/fish/config.fish
+++ shell/config/fish/config.fish
@@ -19,4 +19,8 @@ set -gx AWS_PROFILE ''
 fish_add_path $HOMEBREW_PREFIX/bin
 fish_add_path $HOME/.bin
 
+if test -e $HOME/.dotfiles-custom/config/fish/config.fish
+    source $HOME/.dotfiles-custom/config/fish/config.fish
+end
+
 starship init fish | source
