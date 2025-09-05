c444c9111c34e8d7f616827173dfc92a071ac300
Michael Rödel <hello@mroedel.de>
2025-09-05 11:20:40 +0200

    feat(fish): add kube-get-all-of

---
+++ shell/config/fish/functions/kube-get-all-of.fish
@@ -0,0 +1,16 @@
+function kube-get-all-of --description "kube-get-all-of <resource> -- Get <resource> of all Kubernetes namespaces"
+    if test (count $argv) -ne 1
+        echo "Usage: kube-get-all-of <resource>" >&2
+        return 2
+    else
+        set resource $argv[1]
+    end
+
+    for namespace in $(kubectl get namespaces -o name | awk -F '/' '{print $2}' | sort | uniq)
+        if test (kubectl auth can-i get $resource --namespace $namespace) = 'yes'
+            echo "Namespace:" $namespace
+            kubectl get --ignore-not-found $resource --namespace $namespace
+            echo ""
+        end
+    end
+end
