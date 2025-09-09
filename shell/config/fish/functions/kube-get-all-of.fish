9775cdc6ab1bca3e242439fc41424eb0de33ddf8
Michael Rödel <hello@mroedel.de>
2025-09-09 15:10:47 +0200

    feat(fish): better handling

--- shell/config/fish/functions/kube-get-all-of.fish
+++ shell/config/fish/functions/kube-get-all-of.fish
@@ -8,9 +8,11 @@ function kube-get-all-of --description "kube-get-all-of <resource> -- Get <resou
 
     for namespace in $(kubectl get namespaces -o name | awk -F '/' '{print $2}' | sort | uniq)
         if test (kubectl auth can-i get $resource --namespace $namespace) = 'yes'
-            echo "Namespace:" $namespace
-            kubectl get --ignore-not-found $resource --namespace $namespace
-            echo ""
+            if test (kubectl get --ignore-not-found $resource --namespace $namespace | wc -l) -gt 0
+                echo "Namespace:" $namespace
+                kubectl get --ignore-not-found $resource --namespace $namespace
+                echo ""
+            end
         end
     end
 end
